68e43e71ae98c05ac48ca01d681f10687a877395
belombo <ext.blaise.elombo@fdj-gs.com>
2022-03-19 11:23:14 +0100

    Add calculator class

---
+++ Program.cs
@@ -0,0 +1,13 @@
+namespace ProGitForProgrammers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.WriteLine("Hello World!");
+           Console.WriteLine("I just added this in Visual Studio");
+		   Console.WriteLine("I just added this in the command line repo");
+
+        }
+    }
+}
